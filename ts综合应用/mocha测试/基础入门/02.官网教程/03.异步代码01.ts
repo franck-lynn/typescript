e96e4f6d08d519640dc689033f88b0fc27a390ca
franck-lynn <lry_demry@163.com>
2021-11-21 00:56:33 +0800

    mocha

---
+++ ts综合应用/mocha测试/基础入门/02.官网教程/03.异步代码01.ts
@@ -0,0 +1,31 @@
+
+
+import mongoose, {model, Schema, PopulatedDoc, Document, Query, Model} from "mongoose"
+
+const url = "mongodb://localhost:27017/test"
+
+
+mongoose.connect(url, () => console.log("数据库连接成功"))
+mongoose.connection.on("error", console.error.bind(console, "mongoDB连接异常"))
+
+interface IUser {
+    name: string
+}
+
+const userSchema = new Schema<IUser>({
+    name: {type: String, required: true}
+})
+const User = model<IUser>("User", userSchema)
+
+
+describe('User', function (){
+    describe("#save()", function (){
+        it('should save without error', function (done){
+            const user = new User({name: 'Luna'})
+            user.save((err: mongoose.CallbackError ) => {
+                if(err) done(err)
+                else done()
+            })
+        })
+    })
+})
