a676d8218c6de54bba48ae2aa7aa3c8bef7ddfe9
franck-lynn <lry_demry@163.com>
2021-12-09 21:54:51 +0800

    u

--- ts服务器端/01.koa基础/05.配置路由/目录优化/server/contstants/index.ts
+++ ts服务器端/01.koa基础/05.配置路由/目录优化/server/contstants/index.ts
@@ -7,8 +7,10 @@
 // 获取环境配置中的变量
 import dotenv from "dotenv"
 
-//! 这里的路径是以 app.ts 入口文件的路径开始计算的
-dotenv.config({ path: "../../.env" })
+//! 这里的路径是以入口文件的路径开始计算的
+//! 如果入口文件是 main.ts, 那么, 这个路径就从 main.ts 
+//! 这个文件开始计算
+dotenv.config({ path: "../.env" })
 
 const { HOST_NAME, PORT } = process.env
 export { HOST_NAME, PORT }
