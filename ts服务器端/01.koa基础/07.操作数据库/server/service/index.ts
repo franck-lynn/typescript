5817f042d8e5bc1ca1b030e939b6ec2dafed66f4
franck-lynn <lry_demry@163.com>
2021-12-10 11:27:59 +0800

    package.json

--- ts服务器端/01.koa基础/07.操作数据库/server/service/index.ts
+++ ts服务器端/01.koa基础/07.操作数据库/server/service/index.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose"
 import { MONGO_URI } from "../contstants"
 
-const connectMongoDB = (): void => {
+const connectMongoDB = () => {
     console.log("准备连接数据库", MONGO_URI)
-    // mongoose.connect(MONGO_URI, () => console.log("数据库连接成功"))
-    // // 错误信息, 绑定错误信息处理, 以便定位错误,
-    // mongoose.connection.on("error", console.error.bind(console, "mongoDB连接异常"))
+    mongoose.connect(MONGO_URI, () => console.log("数据库连接成功"))
+    // 错误信息, 绑定错误信息处理, 以便定位错误,
+    mongoose.connection.on("error", console.error.bind(console, "mongoDB连接异常"))
 }
 
 export {connectMongoDB}
