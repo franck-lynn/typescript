4e4788e653dc826e49ea6825dfed02b6338b3f85
franck-lynn <lry_demry@163.com>
2021-12-12 00:55:59 +0800

    mocha

--- ts综合应用/ts测试/mocha入门/02.官方教程/test/user.service.test.ts
+++ ts综合应用/ts测试/mocha入门/02.官方教程/test/user.service.test.ts
@@ -3,22 +3,22 @@ import mongoose from "mongoose"
 import { User } from "src/models/user.model"
 import { createUser } from "../src/services/user.service"
 
-beforeEach(async () => {
+before(async () => {
     const uri = "mongodb://localhost:27017/test"
     mongoose.connect(uri, () => console.log("数据库连接成功"))
     mongoose.connection.on("error", console.error.bind(console, "mongoDB连接异常"))
 })
 
-describe("User 数据库文档", function () {
-    describe("#createUser() 方法", function () {
-        it("创建一个新用户", async () => {
-            const user = new User({ name: "aaaa" })
-            const u = await createUser(user)
-            expect(u.name).to.be.equal('aaaa') 
+describe("User 的数据库文档", function () {
+    describe("一. 测试 createUser() 方法", function () {
+        it("01. 创建一个新用户", async () => {
+            const aUser = new User({ name: "aaaa" })
+            const user = await createUser(aUser)
+            expect(user.name).to.be.equal('aaaa') 
         })
     })
 })
 
-afterEach(async () => {
+after(async () => {
     mongoose.disconnect()
 })
