195604610ebbe41d439a84fb31538bfddb1d4572
franck-lynn <lry_demry@163.com>
2021-12-12 17:23:52 +0800

    aaa

--- ts综合应用/ts测试/mocha入门/02.官方教程/test/stack.test.ts
+++ ts综合应用/ts测试/mocha入门/02.官方教程/test/stack.test.ts
@@ -1,7 +1,7 @@
 import {expect} from 'chai'
 import { Stack } from 'src/stack/Stack'
 
-describe('Stack', () => {
+describe('tack.ts 文件测试', () => {
     it('没有参数也可以初始化', () => {
         const s = new Stack<number>()
         expect(s.size()).to.be.equal(0)
--- ts综合应用/ts测试/mocha入门/02.官方教程/test/user.service.test.ts
+++ ts综合应用/ts测试/mocha入门/02.官方教程/test/user.service.test.ts
@@ -10,16 +10,16 @@ before(async () => {
     mongoose.connection.on("error", console.error.bind(console, "mongoDB连接异常"))
 })
 
-describe("User 的数据库文档", function () {
+describe("User 的数据库文档测试", function () {
     
     describe("一. 测试 createUser() 方法", function () {
-        /* 
+        
         it("01. 创建一个新用户", async () => {
             const aUser = new User({ name: "aaaa" })
             const user = await createUser(aUser)
             expect(user.name).to.be.equal('aaaa') 
         })
-         */
+        
         it("02. 查询一个用户", async () => {
             const name = 'zzr'
             const user = await findUser(name)
