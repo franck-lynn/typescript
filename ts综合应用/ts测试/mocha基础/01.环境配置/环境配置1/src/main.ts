4fe916e252292f45725881951c52d9e02db53508
franck-lynn <lry_demry@163.com>
2021-12-12 14:39:36 +0800

    delete

--- ts综合应用/ts测试/mocha基础/01.环境配置/环境配置1/src/main.ts
+++
@@ -1,4 +0,0 @@
-const add = <T extends string|number>(a: T, b: T): T extends string? string: number => <any> a + b
-
-const minus = (a: number, b: number): number => a - b
-export {add, minus}
--- ts综合应用/ts测试/mocha基础/02.官网教程/起步 quick start/dist/02.检测多个回调.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const done = () => {
-    console.log("回调函数");
-};
-it("double done()", function (done) {
-    // 检测多个回调
-    setImmediate(done);
-    setImmediate(done);
-});
